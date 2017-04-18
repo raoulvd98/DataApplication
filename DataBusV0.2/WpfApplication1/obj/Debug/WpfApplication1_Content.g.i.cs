acd1f2b715620bc17409f1fac434d5384d5fa3b9
jeroen-schep@hotmail.com <jeroen-schep@hotmail.com>
2017-04-18 11:27:34 +0200

    Small changes

--- DataBusV0.2/WpfApplication1/obj/Debug/WpfApplication1_Content.g.i.cs
+++ DataBusV0.2/WpfApplication1/obj/Debug/WpfApplication1_Content.g.i.cs
@@ -8,7 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("kaart_wijken_rotterdam.png")]
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("rotterdam-2.jpg")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("logodatabus.png")]
 
 
